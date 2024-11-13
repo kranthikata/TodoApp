3b017e66dd1b568059e370b8ff1902e5c1bd73d5
kranthikata <kranthikumarkata464@gmail.com>
2024-11-13 10:19:17 +0530

    Feature: Added user CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connectDB = require("./config/connect");
 const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(cors());
@@ -17,6 +18,7 @@ const startServer = async () => {
 
     app.use("/api/auth", authRoutes);
     app.use("/api/todos", todoRoutes);
+    app.use("/api/user/profile", userRoutes);
 
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
