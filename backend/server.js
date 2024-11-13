aec09c1099eb7379f3d968461d60b81054b9c6ad
kranthikata <kranthikumarkata464@gmail.com>
2024-11-13 14:53:00 +0530

    Update: Updated server with the deployment logic

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/connect");
 const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
@@ -10,6 +11,21 @@ const userRoutes = require("./routes/userRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// ------------------------------------Deployment logic-------------------------------------
+const __dirname1 = path.resolve();
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("App running successfully");
+  });
+}
+// ------------------------------------Deployment logic End-------------------------------------
 
 const startServer = async () => {
   try {
